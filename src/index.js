1ad746421bee1f00ec900ca5804ebf294887dd85
JehyunRa <jra4862@gmail.com>
2020-02-26 08:36:34 -0500

    modifying url info to work for netlify deployment 2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Application from "components/Application";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   Axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+} else {
+  Axios.defaults.baseURL = 'http://localhost:8001/';
 }
 
 ReactDOM.render(<Application />, document.getElementById("root"));
